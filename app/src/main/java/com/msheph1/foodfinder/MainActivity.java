6b0bbee006dbf88cc799b8418ab8225b196f3837
Msheph1 <max.r.shep@gmail.com>
2024-02-19 21:35:48 -0600

    rebasing

--- app/src/main/java/com/msheph1/foodfinder/MainActivity.java
+++ app/src/main/java/com/msheph1/foodfinder/MainActivity.java
@@ -10,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
+=======
+        //look into google nearby search advanced
+        // place details
+        //place photos
+        /*
+        String apiKey = BuildConfig.PLACES_API_KEY;
+
+        if(TextUtils.isEmpty(apiKey) || apiKey.equals("DEFAULT_API_KEY"))
+        {
+            Log.e("Places test","No api key");
+            finish();
+            return;
+        }
+
+
+        Places.initializeWithNewPlacesApiEnabled(getApplicationContext(), apiKey);
+
+        PlacesClient placesClient = Places.createClient(this);
+
+         */
+>>>>>>> abf7d80 (rebasing)
     }
 }
