15b22563f56d5258223057e4e8401d8ff63d85e6
Msheph1 <max.r.shep@gmail.com>
2024-02-20 20:16:13 -0600

    first commit

--- app/src/main/java/com/msheph1/foodfinder/MainActivity.java
+++ app/src/main/java/com/msheph1/foodfinder/MainActivity.java
@@ -3,6 +3,11 @@ package com.msheph1.foodfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,12 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
         //look into google nearby search advanced
         // place details
         //place photos
-        /*
         String apiKey = BuildConfig.PLACES_API_KEY;
 
         if(TextUtils.isEmpty(apiKey) || apiKey.equals("DEFAULT_API_KEY"))
@@ -29,8 +31,5 @@ public class MainActivity extends AppCompatActivity {
         Places.initializeWithNewPlacesApiEnabled(getApplicationContext(), apiKey);
 
         PlacesClient placesClient = Places.createClient(this);
-
-         */
->>>>>>> abf7d80 (rebasing)
     }
 }
