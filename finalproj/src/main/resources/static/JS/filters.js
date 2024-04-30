e70d45aee1d9781da32d08d07c0c0f58c47f229c
Msheph1 <max.r.shep@gmail.com>
2024-04-30 16:50:08 -0500

    tweaked filters to be validated

--- finalproj/src/main/resources/static/JS/filters.js
+++ finalproj/src/main/resources/static/JS/filters.js
@@ -15,21 +15,7 @@ function getlocation() {
 }
 
 function validate(){
-  var lati = document.getElementById("lati").value;
-  var longi = document.getElementById("lngi").value;
-  var dist = document.getElementById("dist").value;
-    if (isNaN(lati) || lati > 180 ||  lati < -180) {
-    alert("latitude is invalid");
-    return false;
-}  else if (isNaN(longi) || longi > 180 ||  longi < -180) {
-    alert("longitude is invalid");
-    return false;
-}  else if (isNaN(dist) || dist > 50000 ||  dist < 0) {
-  alert("distance is invalid");
-  return false;
-}
 formsub();
-return true;
 
 }
 
